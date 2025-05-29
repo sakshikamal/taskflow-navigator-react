32c7a6be7a765940017d3f1b1e55decb58d1eb5e
Sakshi Kamal <sakshikamal99@gmail.com>
2025-05-29 10:45:16 -0700

    Removed reoptimize button

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -711,12 +711,7 @@ export default function Homepage() {
                   </div>
                 )}
               </div>
-              <button
-                    onClick={handleDynamicSchedule}
-                    className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                  >
-                    Reoptimize Schedule
-                  </button>
+              
             </div>
 
             <div>
